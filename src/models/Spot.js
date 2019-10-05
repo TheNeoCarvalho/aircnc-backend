9e02281a8fcc7818ef5bd593ebdcb85c47dc8002
Manoel Carvalho <manoelmecat@gmail.com>
2019-10-05 19:17:44 -0300

    Path URL - Imagens

--- src/models/Spot.js
+++ src/models/Spot.js
@@ -19,7 +19,7 @@ const SpotSchema = new mongoose.Schema(
 );
 
 SpotSchema.virtual('thumbnail_url').get(function() {
-  return `http://localhost:3003/files/${this.thumbnail}`;
+  return `https://aircnc-backend-yxkwifzlti.now.sh/files/${this.thumbnail}`;
 });
 
 module.exports = mongoose.model('Spot', SpotSchema);
